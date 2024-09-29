5cf8dce412ce52692cd848eef0a1fe0ba5d69069
tact1m4n3 <tudor@dicu.org>
2024-09-29 22:49:01 +0300

    lsp changes

--- base/.config/nvim/lua/toto/plugins/lsp.lua
+++ base/.config/nvim/lua/toto/plugins/lsp.lua
@@ -108,7 +108,6 @@ return {
     ft = { "rust" },
     config = function()
       vim.g.rustfmt_autosave = 1
-      vim.g.rustfmt_options = "--edition 2021"
       vim.g.rustfmt_emit_files = 1
       vim.g.rustfmt_fail_silently = 0
     end
