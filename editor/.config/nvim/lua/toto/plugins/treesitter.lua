aec5959e63122396bed07c237a81dde6eacb47d8
tact1m4n3 <tudor@dicu.org>
2025-05-17 20:52:22 +0300

    more changes

--- editor/.config/nvim/lua/toto/plugins/treesitter.lua
+++ editor/.config/nvim/lua/toto/plugins/treesitter.lua
@@ -1,47 +1,69 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    -- version = "*",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "bibtex",
-                "c",
-                "comment",
-                "cpp",
-                "go",
-                "gomod",
-                "gowork",
-                "gotmpl",
-                "latex",
-                "lua",
-                "luadoc",
-                "luap",
-                "markdown",
-                "python",
-                "rust",
-                "toml",
-                "vim",
-                "vimdoc",
-                "wgsl",
-                "zig",
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-            textobjects = {
-                move = {
-                    enable = true,
-                    goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-                    goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-                    goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        -- version = "*",
+        event = { "BufReadPre", "BufNewFile" },
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "bibtex",
+                    "c",
+                    "comment",
+                    "cpp",
+                    "go",
+                    "gomod",
+                    "gowork",
+                    "gotmpl",
+                    "latex",
+                    "lua",
+                    "luadoc",
+                    "luap",
+                    "markdown",
+                    "python",
+                    "rust",
+                    "toml",
+                    "vim",
+                    "vimdoc",
+                    "wgsl",
+                    "zig",
                 },
-            },
-        })
-    end,
+                highlight = { enable = true },
+                indent = { enable = true },
+                textobjects = {
+                    move = {
+                        enable = true,
+                        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+                        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+                        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+                        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require 'treesitter-context'.setup({
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                multiwindow = false, -- Enable multiwindow support.
+                max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 1, -- Maximum number of lines to show for a single context
+                trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+                -- Separator between context and content. Should be a single character string, like '-'.
+                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                separator = nil,
+                zindex = 20, -- The Z-index of the context window
+                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+            })
+        end
+    },
 }
